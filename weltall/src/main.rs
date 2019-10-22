d89aea15c10cb5b3396fc02be6bb09aac6c3d938
Sam Renick <samcrenick@gmail.com>
2019-10-22 12:56:00 -0400

    tweak to allow multiline snippets

--- weltall/src/main.rs
+++ weltall/src/main.rs
@@ -1,8 +1,6 @@
 use std::env;
 use std::error::Error;
-use std::fs::File;
-use std::io::{prelude::*, BufReader};
-use std::path::Path;
+use std::fs;
 use std::process;
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -17,20 +15,13 @@ fn run() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let snippets_path: &str = &args[1];
     let search_input: &str = &args[2].trim();
-    let lines = lines_from_file(snippets_path);
-    for line in lines {
-        if line.contains(search_input) {
-            print!("{}", line);
+    let output = fs::read_to_string(snippets_path)?;
+    let snippets = output.split("%~\n");
+    for snip in snippets {
+        if snip.contains(search_input) {
+            print!("{}", snip);
             break;
         }
     }
     Ok(())
-}
-
-fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
-    let file = File::open(filename).expect("no such file");
-    let buf = BufReader::new(file);
-    buf.lines()
-        .map(|l| l.expect("Could not parse line"))
-        .collect()
 }
