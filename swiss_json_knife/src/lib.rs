2ea33f2b147c06939ce67236270d54dc58d1789e
Sam Renick <samcrenick@gmail.com>
2019-11-18 14:43:37 -0500

    split into some funcs

--- swiss_json_knife/src/lib.rs
+++ swiss_json_knife/src/lib.rs
@@ -1,17 +1,13 @@
 use actix_web::{web, HttpResponse};
-// use serde::{Deserialize, Serialize};
 use serde_json::{Result, Value};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 
 pub fn index(input: web::Path<(String, String)>) -> Result<HttpResponse> {
-    let split = input.1.split(",");
-    let mut columns = Vec::new();
-    for s in split {
-        columns.push(s.to_string());
-    }
-    let data = get_string_from_file(format!("/tmp/{}", input.0));
+    let resolved_filename = get_filename(&input.0);
+    let columns = get_columns(&input.1);
+    let data = get_string_from_file(resolved_filename);
     let json_input: Value = serde_json::from_str({ &data }).expect("unable to parse");
     let json_object = json_input.as_object().unwrap();
     let mut output_container = Vec::new();
@@ -22,7 +18,7 @@ pub fn index(input: web::Path<(String, String)>) -> Result<HttpResponse> {
                 if value.is_object() {
                     let sub_object = value.as_object().unwrap();
                     for (sub_key, sub_val) in sub_object.iter() {
-                        if columns.contains(sub_key) {
+                        if columns.contains(sub_key) || columns[0] == "-" {
                             output.insert(sub_key.to_string(), sub_val);
                         }
                     }
@@ -44,4 +40,23 @@ pub fn get_string_from_file(filename: String) -> String {
     let mut data = String::new();
     json_file.read_to_string(&mut data).expect("wtf");
     data
+}
+
+pub fn get_columns(input: &String) -> Vec<String> {
+    let split = input.split(",");
+    let mut columns = Vec::new();
+    for s in split {
+        columns.push(s.to_string());
+    }
+    columns
+}
+
+pub fn get_filename(input: &String) -> String {
+    let filename_split = input.split("-");
+    let mut resolved_filename = String::new();
+    for s in filename_split {
+        resolved_filename.push_str("/");
+        resolved_filename.push_str(s);
+    }
+    resolved_filename
 }
